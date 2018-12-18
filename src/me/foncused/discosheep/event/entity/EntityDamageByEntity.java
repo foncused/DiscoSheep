2c552af28b2bec6e316170d64a3f829a1670e5bd
foncused <foncused@users.noreply.github.com>
2018-12-18 18:49:50 -0500

    Removed redundancy
    Removed redundancy

--- src/me/foncused/discosheep/event/entity/EntityDamageByEntity.java
+++ src/me/foncused/discosheep/event/entity/EntityDamageByEntity.java
@@ -33,68 +33,70 @@ public class EntityDamageByEntity implements Listener {
 
 	@EventHandler
 	public void onEntityDamageByEntity(final EntityDamageByEntityEvent event) {
-		final Entity damaged = event.getEntity();
 		final Entity damager = event.getDamager();
-		if(damaged instanceof Sheep && event.getDamager() instanceof Player) {
+		if(damager instanceof Player) {
 			final Player player = (Player) damager;
 			if(!(player.hasPermission("discosheep.damage"))) {
 				return;
 			}
-			final Sheep sheep = (Sheep) damaged;
-			event.setDamage(this.damage);
-			if(this.damage == 0.0) {
-				sheep.setInvulnerable(true);
-			}
-			if(this.rocket) {
-				new BukkitRunnable() {
-					public void run() {
-						sheep.setVelocity(new Vector(0, 3, 0));
-						new BukkitRunnable() {
-							int count = 10;
-							public void run() {
-								if(count == 0) {
-									this.cancel();
+			final Entity damaged = event.getEntity();
+			if(damaged instanceof Sheep) {
+				final Sheep sheep = (Sheep) damaged;
+				event.setDamage(this.damage);
+				if(this.damage == 0.0) {
+					sheep.setInvulnerable(true);
+				}
+				if(this.rocket) {
+					new BukkitRunnable() {
+						public void run() {
+							sheep.setVelocity(new Vector(0, 3, 0));
+							new BukkitRunnable() {
+								int count = 10;
+								public void run() {
+									if(count == 0) {
+										this.cancel();
+									}
+									final World world = sheep.getWorld();
+									final Location location = sheep.getLocation();
+									world.playSound(
+											location,
+											Sound.ENTITY_FIREWORK_ROCKET_LAUNCH,
+											1F,
+											1F
+									);
+									world.spawnParticle(
+											Particle.EXPLOSION_LARGE,
+											location,
+											1,
+											0,
+											0,
+											0
+									);
+									count--;
 								}
-								final World world = sheep.getWorld();
-								final Location location = sheep.getLocation();
-								world.playSound(
-										location,
-										Sound.ENTITY_FIREWORK_ROCKET_LAUNCH,
-										1F,
-										1F
-								);
-								world.spawnParticle(
-										Particle.EXPLOSION_LARGE,
-										location,
-										1,
-										0,
-										0,
-										0
-								);
-								count--;
-							}
-						}.runTaskTimer(plugin, 0, 3);
-					}
-				}.runTaskLater(this.plugin, 1);
-			}
-			final String uuid = sheep.getUniqueId().toString();
-			if(this.sheeps.add(uuid)) {
-				sheep.setGlowing(this.glow);
-				new BukkitRunnable() {
-					final DyeColor[] colors = DyeColor.values();
-					int i = 0;
-					public void run() {
-						if(sheep.isValid() && (!(sheep.isDead()))) {
-							if(i == colors.length) {
-								i = 0;
-							}
-							sheep.setColor(colors[i]);
-							i++;
-						} else {
-							sheeps.remove(uuid);
+							}.runTaskTimer(plugin, 0, 3);
 						}
-					}
-				}.runTaskTimer(this.plugin, 0, this.speed);
+					}.runTaskLater(this.plugin, 1);
+				}
+				final String uuid = sheep.getUniqueId().toString();
+				if(this.sheeps.add(uuid)) {
+					sheep.setGlowing(this.glow);
+					new BukkitRunnable() {
+						final DyeColor[] colors = DyeColor.values();
+						int i = 0;
+						public void run() {
+							if(sheep.isValid() && (!(sheep.isDead()))) {
+								if(i == colors.length) {
+									i = 0;
+								}
+								sheep.setColor(colors[i]);
+								i++;
+							} else {
+								sheeps.remove(uuid);
+							}
+						}
+					}.runTaskTimer(this.plugin, 0, this.speed);
+				}
 			}
 		}
 	}
